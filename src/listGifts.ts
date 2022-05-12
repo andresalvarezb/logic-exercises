aa298856b920dc4a07b74a7d86819070c524375e
Andres Alvarez <andres.alvarez094@hotmail.es>
2022-05-12 17:08:27 -0500

    feat: completed second challenge

---
+++ src/listGifts.ts
@@ -0,0 +1,19 @@
+const email = 'bicycle car ball _playstation bicycle car teddy'
+
+const gifts: {} = {}
+
+function splitEmail(text: string) {
+	const list = text.trim().split(' ')
+	const listFilter = list.filter(e => !e.startsWith('_'))
+
+	for (const toy of listFilter) {
+		if (gifts.hasOwnProperty(toy)) {
+			gifts[toy] += 1
+		} else {
+			gifts[toy] = 1
+		}
+	}
+	console.log(gifts);
+}
+
+splitEmail(email)
